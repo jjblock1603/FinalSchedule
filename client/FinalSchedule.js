85e5d82def9eaca7f965c9065f240a9fde2e49ee
jjblock1603 <jasonblock15@berkeley.edu>
2016-04-30 20:54:35 +0000

    Saturday

--- client/FinalSchedule.js
+++ client/FinalSchedule.js
@@ -5,12 +5,15 @@
 
         const form = event.target;
         const classTime = form.ClassTimeForm.value;
+        Session.set("time",classTime)
+
+
 
        if (form.Monday.checked)
          {
         const submittedClassDate = form.Monday.value;
          console.log("This is " + submittedClassDate)
-         ClassScheduleCollection.insert({ClassDate: submittedClassDate, createdAt: new Date ()  });
+         Session.set("date", submittedClassDate)
 
         }
 
@@ -18,38 +21,73 @@
          {
            const submittedClassDate = form.Tuesday.value;
            console.log("This is " + submittedClassDate)
-           ClassScheduleCollection.insert({ClassDate: submittedClassDate, createdAt: new Date ()  });
+           Session.set("date", submittedClassDate)
         }
        if (form.Wednesday.checked)
          {
           const submittedClassDate = form.Wednesday.value;
            console.log("This is " + submittedClassDate)
-           ClassScheduleCollection.insert({ClassDate: submittedClassDate, createdAt: new Date ()  });
+           Session.set("date", submittedClassDate)
         }
 
        if (form.Thursday.checked)
          {
           const submittedClassDate = form.Thursday.value;
            console.log("This is " + submittedClassDate)
-           ClassScheduleCollection.insert({ClassDate: submittedClassDate, createdAt: new Date ()  });
+            Session.set("date", submittedClassDate)
         }
 
        if (form.Friday.checked)
          {
           const submittedClassDate = form.Friday.value;
           console.log("This is " + submittedClassDate)
-          ClassScheduleCollection.insert({ClassDate: submittedClassDate, createdAt: new Date ()  });
+          Session.set("date", submittedClassDate)
         }
 
        if (form.SatSun.checked)
          {
           const submittedClassDate = form.SatSun.value;
          console.log("This is " + submittedClassDate)
-         ClassScheduleCollection.insert({ClassDate: submittedClassDate, createdAt: new Date () });
+         Session.set("date", submittedClassDate)
         }
 
-      TimeScheduleCollection.insert({
-      submittedTime: classTime });
+         if (form.Tuesday.checked && form.Thursday.checked)
+          {
+            const submittedClassDates = "TuTh"
+            console.log("This is " + submittedClassDates)
+            Session.set("date", submittedClassDates)
+          }
+
+        if (form.Monday.checked && form.Wednesday.checked && form.Friday.checked)
+          {
+            const submittedClassDates = "MWF"
+            console.log("This is " + submittedClassDates)
+            Session.set("date", submittedClassDates)
+          }
+
+       if (form.Monday.checked && form.Tuesday.checked && form.Wednesday.checked && form.Thursday.checked && form.Friday.checked)
+          {
+            const submittedClassDates = "MTuWThF"
+            console.log("This is " + submittedClassDates)
+            Session.set("date", submittedClassDates)
+          }
+ var x= Session.get("date");
+       
+  if (x== "MTuWThF")
+      {
+      
+      }
+  if (x== "MWF")
+      {
+      
+      }
+    if (x== "TuTh")
+      {
+      
+      }   
+
+   //   TimeScheduleCollection.insert({
+  //    submittedTime: classTime });
      }
   }
      )
